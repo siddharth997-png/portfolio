b2f5505b7432d8802f492ba8de583be0a8573e56
Siddharth Kothari <siddharthkothari655@gmail.com>
2021-09-21 13:59:42 +0530

    added dynamic projects rendering using useEffects

--- src/pages/projects/project-data.js
+++ src/pages/projects/project-data.js
@@ -27,4 +27,42 @@ export const allProjectsData = [
     employer: 'Personal',
     desc: 'This web application supports real-time bi-directional communication, which makes it a great fit for a chat app.'
   }
+];
+
+export const reactProjects = [
+  {
+    id: 1,
+    imgSrc: 'assets/shopping.svg',
+    projectTitle: 'E Commerce Application',
+    employer: 'Personal',
+    desc: 'A fully fledged E-Commerce website with Sign In and Goggle O-Authentication feature.'
+  }
+];
+
+export const nodeProjects = [
+  {
+    id: 1,
+    imgSrc: 'assets/chat.svg',
+    projectTitle: 'Real Time Chat Application',
+    employer: 'Personal',
+    desc: 'This web application supports real-time bi-directional communication, which makes it a great fit for a chat app.'
+  }
+];
+
+export const phpProjects = [
+  {
+    id: 1,
+    imgSrc: 'assets/booking.svg',
+    projectTitle: 'Employee Leave Pass and Visitor Booking',
+    employer: 'Aakar Foundry Pvt Ltd',
+    desc: 'This web application brings entire visitor appointment booking, authentication and employee leave pass generation for over 400+ employees online.',  
+  },
+  {
+    id: 2,
+    imgSrc: 'assets/data-reports.svg',
+    projectTitle: 'Measurement System Analysis',
+    employer: 'Fluid Control Pvt Ltd',
+    desc: 'This web application helps identify, catalog, calibrate and study the measurement certainty of inspection, testing, and lab equipment.'
+  }
+
 ]
--- src/pages/projects/projects.component.jsx
+++ src/pages/projects/projects.component.jsx
@@ -1,27 +1,60 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+//import Zoom from 'react-reveal/Zoom';
 
 import ProjectMenu from '../../components/project-menu/project-menu.component';
 import ProjectCard from '../../components/project-card/project-card.component';
 
 import './projects.styles.scss';
 
-import {allProjectsData} from './project-data';
+import {
+  allProjectsData,
+  nodeProjects,
+  reactProjects,
+  phpProjects } from './project-data';
 
 const Projects = () => {
 
   const [selected,setSelected] = useState('all');
+  const [data,setData] = useState([]);
+
+  useEffect(() => {
+    switch(selected) {
+      case 'all' : {
+        setData(allProjectsData);
+          break;
+      }
+      case 'react' : {
+        setData(reactProjects);
+        break;
+      }
+      case 'node' : {
+        setData(nodeProjects);
+        break;
+      }
+      case 'php' : {
+        setData(phpProjects);
+        break;
+      }
+
+      default: {
+        setData([]);
+      }
+    }
+  },[selected])
 
   return (
     <div className='projects'>
       <h1 className='projects__heading'>My Projects</h1>
       <ProjectMenu selected={selected} setSelected={setSelected}/>
-      <div className='projects__container'>
-        {
-          allProjectsData.map(({id,...otherProps}) => (
-            <ProjectCard key={id} {...otherProps}/>
-          ))
-        }
-      </div>
+        <div className='projects__container'>
+          {
+              data.map(({id,...otherProps}) => (
+              
+                <ProjectCard key={id} {...otherProps}/>
+              
+            ))
+          }
+        </div>
     </div>
   )
 }
